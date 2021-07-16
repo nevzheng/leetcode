1d1aadbe828c1961cdee83ccfa72ab1fd412c645
Nevin Zheng <nevzheng@gmail.com>
2021-07-16 12:24:41 -0400

    Add 167

---
+++ src/167_two_sum_2.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Nevin Zheng on 7/16/21.
+//
+//
+// Created by Nevin Zheng on 7/16/21.
+//
+
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+#include <catch2/catch.hpp>
+
+using namespace std;
+
+// Following from "1_two_sum", we can apply the same solution in O(n) time and O(n) memory
+// However there is another interesting solution that we can apply now that the list is sorted.
+// We iterate over the array. For each i, A[i] we can use binary search to search for the complement
+// tgt - A[i] in the range A[i+1:]
+vector<int> twoSumSorted(const vector<int> &A, int tgt) {
+    for (auto it = begin(A); it != end(A); it++) {
+        auto comp = tgt - *it;// Complement of it
+        if (auto jt = lower_bound(it + 1, end(A), comp); jt != end(A) and *jt == comp) {
+            int idx = (int) distance(begin(A), it) + 1;
+            int jdx = (int) distance(begin(A), jt) + 1;
+            return {idx, jdx};
+        }
+    }
+    return {};
+}
+
+TEST_CASE("167_example1", "[167]") {
+    auto nums = vector{2, 7, 11, 15};
+    auto target = 9;
+    auto expected = vector{1, 2};
+    auto result = twoSumSorted(nums, target);
+    REQUIRE(result == expected);
+}
+
+TEST_CASE("167_example2", "[167]") {
+    auto nums = vector{2, 3, 4};
+    auto target = 6;
+    auto expected = vector{1, 3};
+    auto result = twoSumSorted(nums, target);
+    REQUIRE(result == expected);
+}
+
+TEST_CASE("167_example3", "[167]") {
+    auto nums = vector{-1, 0};
+    auto target = -1;
+    auto expected = vector{1, 2};
+    auto result = twoSumSorted(nums, target);
+    REQUIRE(result == expected);
+}
