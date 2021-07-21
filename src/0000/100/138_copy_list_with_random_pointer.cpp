3cf2e0f690b60744429711a84ff6f0173756581f
Nevin Zheng <nevzheng@gmail.com>
2021-07-21 14:36:10 -0400

    Added 138, splicing method

--- src/0000/100/138_copy_list_with_random_pointer.cpp
+++ src/0000/100/138_copy_list_with_random_pointer.cpp
@@ -31,7 +31,7 @@ public:
  * @param head
  * @return
  */
-Node *copyRandomList(Node *head) {
+Node *copyRandomListHashMap(Node *head) {
     Node *cur{head}, *newCur{nullptr}, *newHead{nullptr};
     map<Node *, Node *> copied;
     while (cur) {
@@ -50,6 +50,45 @@ Node *copyRandomList(Node *head) {
     newCur = newHead;
     while (newCur) {
         if (newCur->random) newCur->random = copied[newCur->random];
+        newCur = newCur->next;
+    }
+    return newHead;
+}
+
+/**
+ * This Solution is pretty clever. It copies the nodes of the old list, splicing the new nodes after the originals.
+ * After the original copy pass, it does and additional two passes to i. fix up the pointers, ii. Splice out the new list
+ * In an interview its worth mentioning both solutions, but the above method is safer and clearer.
+ * O(N) time. Three passes, O(1) time, no additional memory for recording mapping between old and new pointers
+ * @param head
+ * @return
+ */
+Node *copyRandomListSplicing(Node *head) {
+    if (!head) return nullptr;
+    Node *cur{head}, *newHead{nullptr}, *newCur{nullptr};
+    // Make a Copy of the Original List Splicing copies after the originals
+    while (cur) {
+        auto tmp = cur->next;
+        cur->next = new Node(cur->val);
+        cur->next->next = tmp;
+        cur = tmp;
+    }
+    cur = head;
+    // Adjust the random ptrs of the newly added nodes
+    while (cur) {
+        // If we see a spliced cpy
+        if (cur->next) {
+            if (cur->random) cur->next->random = cur->random->next;
+        }
+        // Skip to the next original Node,
+        cur = cur->next ? cur->next->next : cur->next;
+    }
+    // Splice out the original list
+    cur = head, newHead = head->next, newCur = head->next;
+    while (cur && newCur) {
+        cur->next = cur->next ? cur->next->next : cur->next;
+        newCur->next = newCur->next ? newCur->next->next : newCur->next;
+        cur = cur->next;
         newCur = newCur->next;
     }
     return newHead;
@@ -77,7 +116,36 @@ TEST_CASE("138ex1", "[138]") {
 
     one->random = seven;
 
-    auto result = copyRandomList(seven);
+    auto result = copyRandomListHashMap(seven);
+
+    // Verifying is a little tricky, just use leetcode
+    REQUIRE(result != nullptr);
+}
+
+
+TEST_CASE("138ex2", "[138]") {
+    //    Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+    //    Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+    auto seven = new Node(7);
+    auto thirteen = new Node(13);
+    auto eleven = new Node(11);
+    auto ten = new Node(10);
+    auto one = new Node(1);
+
+    seven->next = thirteen;
+
+    thirteen->next = eleven;
+    thirteen->random = seven;
+
+    eleven->next = ten;
+    eleven->random = one;
+
+    ten->next = one;
+    ten->random = eleven;
+
+    one->random = seven;
+
+    auto result = copyRandomListSplicing(seven);
 
     // Verifying is a little tricky, just use leetcode
     REQUIRE(result != nullptr);
