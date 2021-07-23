982558f764751005f54377a34d6c983982b79a77
Nevin Zheng <nevzheng@gmail.com>
2021-07-23 02:03:32 -0400

    + 1584: simpler, faster

--- src/1000/1584_min_cost_to_connect_all_edges.cpp
+++ src/1000/1584_min_cost_to_connect_all_edges.cpp
@@ -8,51 +8,35 @@
 
 using namespace std;
 
-using Cost = int;
-using V = int;
-using Edge = pair<Cost, V>;
-using AdjList = vector<vector<Edge>>;
-using CostMap = map<pair<int, int>, int>;
+using AdjMatrix = vector<vector<int>>;
 
+int findMinIndex(const int n_vertices, const vector<int>& distance,
+                 const vector<bool>& visited) {
+  auto min_index = -1;
+  auto min_val = INT_MAX;
+  for (auto i = 0; i < n_vertices; i++) {
+    if (!visited[i] and distance[i] < min_val) {
+      min_val = distance[i];
+      min_index = i;
+    }
+  }
+  return min_index;
+}
 int minCostConnectPoints(const vector<vector<int>>& points) {
   auto n_vertices = static_cast<int>(points.size());
-  CostMap cost_map;
-  AdjList graph(n_vertices);
-  for (auto i = 0; i < n_vertices - 1; i++) {
-    for (auto j = i + 1; j < n_vertices; j++) {
-      auto [p1, p2] = tie(points[i], points[j]);
+  vector dist(n_vertices, INT_MAX);  // Cost of Including an node in graph
+  dist[0] = 0;                       // Init a vertex in mst to dist 0
+  vector vis(n_vertices, false);     // Has a Node been Included yet
+  for (auto i = 0; i < n_vertices; i++) {
+    auto u = findMinIndex(n_vertices, dist, vis);
+    vis[u] = true;
+    for (auto v = 0; v < n_vertices; v++) {
+      auto [p1, p2] = tie(points[u], points[v]);
       auto cost = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]);
-      graph[i].push_back({cost, j});
-      graph[j].push_back({cost, i});
-      cost_map[{i, j}] = cost;
-      cost_map[{j, i}] = cost;
+      if (!vis[v] and dist[v] > cost) dist[v] = cost;
     }
   }
-  vector<int> costs(n_vertices, INT_MAX);  // Costs
-  vector<int> pred(n_vertices, -1);        // predecessors in the MST
-  vector<bool> included(n_vertices, false);
-  priority_queue<Edge, vector<Edge>, greater<>> pq;
-  pq.push({0, 0});  // Push (cost=0, src=0) onto queue
-  while (!pq.empty()) {
-    auto [_, u] = pq.top();
-    pq.pop();
-    if (included[u]) continue;
-    included[u] = true;
-    for (auto x : graph[u]) {
-      auto [cost, v] = x;
-      if (!included[v] and costs[v] > cost) {
-        costs[v] = cost;
-        pred[v] = u;
-        pq.push({costs[v], v});
-      }
-    }
-  }
-  auto cost = 0;
-  for (auto i = 1; i < n_vertices; i++) {
-    auto [u, v] = tie(i, pred[i]);
-    cost += cost_map[{u, v}];
-  }
-  return cost;
+  return std::accumulate(begin(dist), end(dist), 0);
 }
 
 TEST_CASE("1584ex1", "[1584]") {
