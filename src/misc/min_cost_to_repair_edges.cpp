d8888759e96efa5e0a8d113e403f6759d6aa534e
Nevin Zheng <nevzheng@gmail.com>
2021-07-24 19:52:20 -0400

    Add misc/min_cost_to_repair_edges

--- src/misc/min_cost_to_repair_edges.cpp
+++ src/misc/min_cost_to_repair_edges.cpp
@@ -6,8 +6,76 @@
 #include <bits/stdc++.h>
 #include <catch2/catch.hpp>
 #include <fmt/format.h>
+#include <utility>
 
 using namespace std;
+
+class UnionFind {
+ private:
+  vector<int> ids_;
+  vector<int> sizes_;
+
+ public:
+  /**
+   * Construct a UnionFind initialized with n elements in n distinct sets
+   * @param n
+   */
+  explicit UnionFind(int n) : ids_(vector<int>(n)), sizes_(n, 1) {
+    iota(begin(ids_), end(ids_), 0);
+  }
+
+  /**
+   * Return which root of the component that p belongs to
+   * Follows the tree until a root node is found and returned
+   * @param p
+   * @return
+   */
+  int find(int p) {
+    auto root = p;
+    while (root != ids_[root]) root = ids_[root];
+
+    while (p != ids_[p]) {  // Perform Path Compression
+      auto next = ids_[p];
+      ids_[p] = root;
+      p = next;
+    }
+    return root;
+  }
+
+  /**
+   * Return the size of the component that p belongs to
+   * @param p
+   * @return
+   */
+  int size(int p) { return sizes_[find(p)]; }
+
+  /**
+   * Determine if p and q belong to the same component
+   * @param p
+   * @param q
+   * @return true if p and q are in the same component. Else False.
+   */
+  bool connected(int p, int q) { return find(p) == find(q); }
+
+  /**
+   * Unite components p and q
+   * Uses Union by Rank to limit height of trees
+   * @param p
+   * @param q
+   */
+  void unite(int p, int q) {
+    auto p_root = find(p);
+    auto q_root = find(q);
+    if (sizes_[p_root] < sizes_[q_root]) {
+      sizes_[q_root] += sizes_[p_root];
+      ids_[p_root] = q_root;
+    } else {
+      sizes_[p_root] += sizes_[q_root];
+      ids_[q_root] = p_root;
+    }
+  }
+};
+
 /**
  *
  * @param n_vertices Total number of nodes
@@ -18,7 +86,42 @@ using namespace std;
  * @return Minimum Cost to fully connect the graph
  */
 int costToRepair(int n_vertices, const vector<vector<int>>& edges,
-                 const vector<vector<int>>& edgesToRepair) {}
+                 const vector<vector<int>>& edgesToRepair) {
+  auto min_cost = 0;
+  // Create a set indicating broken edges. Good For fast lookup
+  set<pair<int, int>> broken_edges;
+  for (const auto& e : edgesToRepair) {
+    broken_edges.emplace(e[0] - 1, e[1] - 1);
+    broken_edges.emplace(e[1] - 1, e[0] - 1);
+  }
+  // Vec of edges for kruskal's algo
+  // Cost, U, V
+  vector<tuple<int, int, int>> edge_vec;
+  // add Edges with zero cost representing unbroken edges
+  for (auto e : edges) {
+    if (broken_edges.count({e[0] - 1, e[1] - 1}) == 0U) {
+      edge_vec.emplace_back(0, e[0] - 1, e[1] - 1);
+      edge_vec.emplace_back(0, e[1] - 1, e[0] - 1);
+    }
+  }
+  // Add Broken Edges into to the edge ved
+  transform(begin(edgesToRepair), end(edgesToRepair), back_inserter(edge_vec),
+            [](const auto e) { return make_tuple(e[2], e[0] - 1, e[1] - 1); });
+
+  // Sort the edge vector by Cost, default is compare by first index
+  sort(begin(edge_vec), end(edge_vec));
+  // create unionfind for kruskals
+  UnionFind uf(n_vertices);
+  for (auto& e : edge_vec) {
+    auto [w, u, v] = e;
+    if (uf.connected(u, v)) continue;  // Avoid Cycles
+    min_cost += w;
+    uf.unite(u, v);
+    if (uf.size(0) == n_vertices) break;
+  }
+  if (uf.size(0) != n_vertices) return -1;
+  return min_cost;
+}
 
 TEST_CASE("costToRepairEx1", "[costToRepair]") {
   // Input: n = 5,
