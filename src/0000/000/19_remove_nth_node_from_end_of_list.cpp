187bb5b1900dc9a29fd6601f721b30d1a0361af5
Nevin Zheng <nevzheng@gmail.com>
2021-09-01 22:48:19 -0400

    + 19

--- src/0000/000/19_remove_nth_node_from_end_of_list.cpp
+++ src/0000/000/19_remove_nth_node_from_end_of_list.cpp
@@ -11,7 +11,18 @@ using namespace std;
 
 class Solution {
  public:
-  static ListNode* removeNthFromEnd(ListNode* head, int n) {}
+  static ListNode* removeNthFromEnd(ListNode* head, int n) {
+    auto* fast = head;
+    auto* slow = head;
+    // Len of List is n + (N-n)
+    while (n--) fast = fast->next;  // Set fast to n
+    if (!fast) return head->next;
+    // Move Slow to N-n
+    while (fast->next) fast = fast->next, slow = slow->next;
+    // slow is now at correct position
+    slow->next = slow->next->next;
+    return head;
+  }
 };
 
 TEST_CASE("19::1", "[19]") {
@@ -20,5 +31,10 @@ TEST_CASE("19::1", "[19]") {
   auto three = make_unique<ListNode>(3, four.get());
   auto two = make_unique<ListNode>(2, three.get());
   auto one = make_unique<ListNode>(1, two.get());
-  REQUIRE(Solution::removeNthFromEnd(one) != nullptr);
+  REQUIRE(Solution::removeNthFromEnd(one.get(), 2) != nullptr);
+}
+
+TEST_CASE("19::2", "[19]") {
+  auto one = make_unique<ListNode>(1);
+  REQUIRE(Solution::removeNthFromEnd(one.get(), 1) != nullptr);
 }
